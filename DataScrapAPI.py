00d135a9a3a42c18f0cc952002715f398c7fd7df
Kelvin Kong <ckong9@horizon.csueastbay.edu>
2022-05-18 22:03:00 -0700

    Update DataScrapAPI.py
    update DataScrap API to use Bearer Token instead of using API Keys and Access Tokens

--- DataScrapAPI.py
+++ DataScrapAPI.py
@@ -3,18 +3,9 @@
 ### Last Modified on May 16 2022 ###
 
 ### We Use Twitter API v2 ###
-from tweepy import OAuthHandler
-from tweepy.streaming import Stream #Tweepy Update to import Stream
 import tweepy
-import json
-import pandas as pd
-import csv
-import re
-from textblob import TextBlob
-import string
-import preprocessor as p
-import os
 import time
+from datetime import datetime
 
 # Personal Twitter Dev Credentials
 # developer.twitter.com
@@ -23,9 +14,43 @@ api_key_secret = "api_key_secret"
 
 access_token = "access-token" 
 access_token_secret = "access-token-secret" 
+
+bearerToken = "bearer-token"
 # Obtain Details from Twitter Dev Website
 
-auth = OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Start Program Time: ", current_time)
 
+#### Use this Code if you plan to use api_keys and access_token
+# client = tweepy.Client(
+#    consumer_key = api_key, 
+#    consumer_secret = api_key_secret, 
+#    access_token = accessToken, 
+#    access_token_secret = accessTokenSecret
+#)
+### End of Section
+
+#### Use this code if you plan to use bearer token only
+client = tweepy.Client(bearer_token = bearerToken)
+### End of Section
+
+#Specify Query
+Query = 'iPhone -is:retweet'
+
+results = client.search_recent_tweets(query = Query, tweet_fields = ['context_annotations', 'created_at'], max_result=100) 
+
+# Print Result
+
+for result in results.data:
+    print(result.text)
+    if len(result.context_annotations) > 0:
+        print(result.context_annotations)
+
+
+# Let Tweepy sleep 15mins
+print("Wait the API to response in 15 mins after each run")
+
+for i in range(901):
+    print("Time Elipsed " + str(i+1) + " seconds", end="\r")
+    time.sleep(1) 
